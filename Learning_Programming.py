4ead59c598357b55acb7d86b9d98e7156731a11d
Magesh K <153383096+MageshK08@users.noreply.github.com>
2024-12-24 19:01:28 +0530

    Learning_Programming.py file added

---
+++ Learning_Programming.py
@@ -0,0 +1,13 @@
+'''Prime number'''
+Num=int(input("Enter a number:"))
+if Num<2:
+    print("Not prime")
+else:
+    c=2
+    while c*c<=Num:
+        if Num<c==0:
+            print("Not Prime")
+            break
+        c+=1
+    else:
+        print("Prime")
